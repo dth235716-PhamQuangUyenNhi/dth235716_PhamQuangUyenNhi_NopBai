93ff1e7507a94d30bc2d1ddfe5aa0205872d153a
dth235716-PhamQuangUyenNhi <nhi_dth235716@student.agu.edu.vn>
2025-08-26 20:12:26 +0700

    Add files via upload

---
+++ BaiTapChuong1.py
@@ -0,0 +1,49 @@
+﻿import sys
+
+#bt1.4
+print("Chào các bạn !")
+
+#bt1.5
+ho = input("Nhập họ của bạn: ")
+ten= input("Nhập tên của bạn: ")
+
+print(ho + " " + ten)
+print('\n')
+
+#bt1.6
+print("Ta về mình có nhớ ta?")
+print("Mười lăm năm ấy thiết tha mặn nồng")
+print("Mình về mình có nhớ Không?")
+print("Nhìn cây nhớ núi, nhìn sông nhớ nguồn.")
+print('\n')
+
+#bt1.7
+
+
+#bt1.8
+print("****")
+print("*  *")
+print("*  *")
+print("****")
+print('\n')
+
+#bt1.9
+for i in range(4):
+    for i in range(10):
+        print("*", end=' ')
+    print('\n')
+
+#bt1.10
+
+print(" -------------")
+print("|      *      |")
+print("|     ***     |")
+print("|   *******   |")
+print("|     ***     |")
+print("|    *****    |")
+print("|   *******   |")
+print("| *********** |")
+print("|     * *     |")
+print("|     * *     |")
+print(" -------------")
+print('\n')
